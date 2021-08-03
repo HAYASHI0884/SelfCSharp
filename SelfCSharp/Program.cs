a702c70e7e106e890e559d348b38109a686c7669
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-03 15:25:10 +0900

    最初のコミット

---
+++ SelfCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace SelfCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string value = null;
+            Console.WriteLine(sizeof(int));
+        }
+    }
+}
