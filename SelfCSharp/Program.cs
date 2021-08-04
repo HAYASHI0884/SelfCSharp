63da493ea289477baba4826536e589d9ea03bf27
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-04 16:40:36 +0900

    正規表現の学習

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SelfCSharp
 {
@@ -7,12 +8,17 @@ namespace SelfCSharp
     {
         static void Main()
         {
-            var str = "プログラミング言語";
-            var strings = "鈴木\t太郎\t男\t50歳\t広島県";
-            var result = strings.Split("\t");
+            var str = "住所は〒184-9000 鎌ヶ谷市梶野町0-0-0です。\nあなたの住所は〒273-0000 嬬恋市大野町0-9-9ですね";
+            var rgx = new Regex(@"\d{3}-\d{4}", RegexOptions.Multiline);
+            var match = rgx.Matches(str);
+            foreach (var m in match)
+            {
+                Console.WriteLine(m);
+            };
 
-            Console.WriteLine(str.Substring(4, 3));
-            Console.WriteLine(string.Join(",", result));
+            var str2 = "お問い合わせはhoge@example.comまで";
+            var rgx2 = new Regex(@"[a-z0-9.!#$%&'*+/=?^_{-}~-]+@[a-z0-9-]+(?:¥.[a-z0-9-]+)*", RegexOptions.IgnoreCase);
+            Console.WriteLine(rgx2.Replace(str2, "<a href='mailto:$0'>$0</a>"));
         }
     }
 }
