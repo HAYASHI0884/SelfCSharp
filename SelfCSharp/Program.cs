84e8919ddf8c842bdaae2e17013c1825a33814c1
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-04 17:53:17 +0900

    日付の操作

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace SelfCSharp
 {
@@ -8,17 +6,13 @@ namespace SelfCSharp
     {
         static void Main()
         {
-            var str = "住所は〒184-9000 鎌ヶ谷市梶野町0-0-0です。\nあなたの住所は〒273-0000 嬬恋市大野町0-9-9ですね";
-            var rgx = new Regex(@"\d{3}-\d{4}", RegexOptions.Multiline);
-            var match = rgx.Matches(str);
-            foreach (var m in match)
-            {
-                Console.WriteLine(m);
-            };
+            var date = "2018/02/15 13:17:23";
+            var dt = DateTime.Parse(date);
+            Console.WriteLine(dt.Year);
+            Console.WriteLine(dt.Hour);
 
-            var str2 = "お問い合わせはhoge@example.comまで";
-            var rgx2 = new Regex(@"[a-z0-9.!#$%&'*+/=?^_{-}~-]+@[a-z0-9-]+(?:¥.[a-z0-9-]+)*", RegexOptions.IgnoreCase);
-            Console.WriteLine(rgx2.Replace(str2, "<a href='mailto:$0'>$0</a>"));
+            var atdt = DateTime.Now;
+            Console.WriteLine(atdt.AddDays(15));
         }
     }
 }
