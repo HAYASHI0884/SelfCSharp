48abeffdec67f6f016b7911eac6891c2cbfc7f5a
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-05 15:45:23 +0900

    コレクションの学習

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace SelfCSharp
 {
@@ -8,27 +9,13 @@ namespace SelfCSharp
     {
         static void Main()
         {
-            var str = "となりのきゃくはよくきゃくくうきゃくだ";
-            //var rgx = new Regex("きゃく");
-            //var result = rgx.Matches(str);
-
-            Console.WriteLine(str.LastIndexOf("きゃく"));
-
-            //foreach (Match m in result)
-            //{
-            //    Console.WriteLine(m.Index);
-            //}
-
-            //var str2 = "□△の気温は□△℃です。";
-            //var array = new[] { "弘前", "15.156" };
-            //foreach (var ary in array)
-            //{
-            //    Console.WriteLine(str2.Replace("□△", ary[0]));
-            //}
-
-            var loc = "弘前";
-            var temp = 15.156;
-            Console.WriteLine(String.Format("{0}の気温は{1:F1}", loc, temp));
+            var list = new List<int>
+            {
+                15,
+                23,
+                29,
+                37
+            };
         }
     }
 }
