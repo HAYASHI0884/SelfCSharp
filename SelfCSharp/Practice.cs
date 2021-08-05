d594a4f9ec7a3cf986773fbc7b5ebe4fc7212771
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-05 13:45:54 +0900

    標準ライブラリのまとめ

---
+++ SelfCSharp/Practice.cs
@@ -0,0 +1,9 @@
+﻿//using System.IO;
+
+//namespace SelfCSharp.practice
+//{
+//    class Practice
+//    {
+
+//    }
+//}
--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace SelfCSharp
 {
@@ -6,13 +8,28 @@ namespace SelfCSharp
     {
         static void Main()
         {
-            var date = "2018/02/15 13:17:23";
-            var dt = DateTime.Parse(date);
-            Console.WriteLine(dt.Year);
-            Console.WriteLine(dt.Hour);
+            var str = "となりのきゃくはよくきゃくくうきゃくだ";
+            //var rgx = new Regex("きゃく");
+            //var result = rgx.Matches(str);
 
-            var atdt = DateTime.Now;
-            Console.WriteLine(atdt.AddDays(15));
+            Console.WriteLine(str.LastIndexOf("きゃく"));
+
+            //foreach (Match m in result)
+            //{
+            //    Console.WriteLine(m.Index);
+            //}
+
+            //var str2 = "□△の気温は□△℃です。";
+            //var array = new[] { "弘前", "15.156" };
+            //foreach (var ary in array)
+            //{
+            //    Console.WriteLine(str2.Replace("□△", ary[0]));
+            //}
+
+            var loc = "弘前";
+            var temp = 15.156;
+            Console.WriteLine(String.Format("{0}の気温は{1:F1}", loc, temp));
         }
     }
 }
+
