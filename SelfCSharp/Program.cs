7426167b78fb585c2bebb3733dead5b33b49083a
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-05 17:43:59 +0900

    問題1,2の回答

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -7,18 +7,33 @@ namespace SelfCSharp
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            var list = new List<int>
-            {
-                15,
-                23,
-                29,
-                37
-            };
+            Console.WriteLine("hayashi");
+            Console.Write("123");
+            Console.Write("456");
+            Console.WriteLine("789");
+            Console.WriteLine("こんにちは");
+            Console.WriteLine("{0} + {1} = {2}", 5, 3, 5 + 3);
+            Console.WriteLine("{0} - {1} = {2}", 5, 3, 2);
+            Console.WriteLine("{0} * {1} = {2}", 5, 2, 10);
+            Console.WriteLine("{0} / {1} = {2}", 5, 2, 2);
+            Console.WriteLine("{0} % {1} = {2}", 5, 2, 1);
+            int a = 1, b = 2, c = 3, d = 4, e = 5;
+            a = a + 2;
+            b = b - 1;
+            c = c * 3;
+            d = d / 2;
+            e = e % 2;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            Console.WriteLine(str1 + str2);
         }
     }
-
-    Console.WriteLine("こんにちは！");
 }
 
