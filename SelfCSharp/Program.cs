e51d2b4f0e20772a30f326d48c47a03c65990c9e
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-06 15:49:13 +0900

    ~問題3-11

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Collections.Generic;
 
 namespace SelfCSharp
 {
@@ -9,31 +6,77 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hayashi");
-            Console.Write("123");
-            Console.Write("456");
-            Console.WriteLine("789");
-            Console.WriteLine("こんにちは");
-            Console.WriteLine("{0} + {1} = {2}", 5, 3, 5 + 3);
-            Console.WriteLine("{0} - {1} = {2}", 5, 3, 2);
-            Console.WriteLine("{0} * {1} = {2}", 5, 2, 10);
-            Console.WriteLine("{0} / {1} = {2}", 5, 2, 2);
-            Console.WriteLine("{0} % {1} = {2}", 5, 2, 1);
-            int a = 1, b = 2, c = 3, d = 4, e = 5;
-            a = a + 2;
-            b = b - 1;
-            c = c * 3;
-            d = d / 2;
-            e = e % 2;
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-            string str1 = Console.ReadLine();
-            string str2 = Console.ReadLine();
-            Console.WriteLine(str1 + str2);
+            //Console.WriteLine("i=");
+            //var i = int.Parse(Console.ReadLine());
+            //if (30 <= i && i <= 70)
+            //{
+            //    Console.WriteLine("30より大きく70以下です");
+            //}
+
+            //Console.WriteLine("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //if (a == b)
+            //{
+            //    Console.WriteLine("aはbと等しい");
+            //}
+            //else if (a < b)
+            //{
+            //    Console.WriteLine("aはbよりも小さい");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("aはbよりも大きい");
+            //}
+
+            //Console.WriteLine("num=");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("この数は偶数です。");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("この数は奇数です。");
+            //}
+
+            //Console.WriteLine("季節を入力(1:春 2:夏 3:秋 4:冬)");
+            //int season = int.Parse(Console.ReadLine());
+            //switch (season)
+            //{
+            //    case 1:
+            //        Console.WriteLine("春です");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("夏です");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("秋です");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("冬です");
+            //        break;
+            //    default:
+            //        Console.WriteLine("エラーです");
+            //        break;
+
+            Console.WriteLine("1から100までの数値を入力してください");
+            int Range = int.Parse(Console.ReadLine());
+            if (20 <= Range && Range < 80)
+            {
+                Console.WriteLine("20以上80未満です。");
+            }
+            else if (Range < 0 || 100 < Range)
+            {
+                Console.WriteLine("範囲外です。");
+            }
+            else
+            {
+                Console.WriteLine("20未満か80以上です。");
+            }
         }
+
     }
 }
 
