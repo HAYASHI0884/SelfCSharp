6aee9c96b61df930c83db6b3504b78706f671030
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-10 11:02:36 +0900

    5つの乱数ループ練習

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SelfCSharp
 {
@@ -7,15 +9,34 @@ namespace SelfCSharp
         static void Main(string[] args)
         {
             Random rnd = new System.Random();
-            int result = 1;
 
-            while (result % 10 != 0)
+            int[] a = new int[5];
+            var even = new List<int>();
+            var odd = new List<int>();
+
+            for (int i = 0; i < a.Length; i++)
             {
-                result = rnd.Next(1, 101);
-                Console.WriteLine(result);
+                int result = rnd.Next(1, 101);
+                a[i] = result;
+                Console.WriteLine(a[i]);
+
+                if (result % 2 == 0)
+                {
+                    even.Add(result);
+                }
+                else
+                {
+                    odd.Add(result);
+                }
             }
 
-            Console.WriteLine("終了します");
+            int intMax = a.Max();
+            int intMin = a.Min();
+
+            Console.WriteLine($"最大値:{intMax}");
+            Console.WriteLine($"最小値:{intMin}");
+            Console.WriteLine($"偶数の数{even.Count}");
+            Console.WriteLine($"奇数の数{odd.Count}");
         }
     }
 }
