301b6addc5c442269e8ef9f061658f5a8af28efb
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-10 16:36:21 +0900

    二次元配列の作成

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace SelfCSharp
 {
@@ -9,47 +7,17 @@ namespace SelfCSharp
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            var list = new List<int>(5);
-            var orMore = new List<int>();
-            var orLess = new List<int>();
+            int[,] data = new int[3, 3];
 
-            for (int i = 0; i < list.Capacity; i++)
+            for (int i = 0; i < 3; i++)
             {
-                int result = rnd.Next(1, 11);
-                list.Add(result);
-                Console.Write($"{result} ");
-            }
-
-            int listAverage = (int)list.Average();
-
-            Console.WriteLine();
-            Console.WriteLine($"合計値 : {list.Sum()}");
-            Console.WriteLine($"平均値 : {listAverage}");
-
-            foreach (int num in list)
-            {
-                if (listAverage < num)
+                for (int j = 0; j < 3; j++)
                 {
-                    orMore.Add(num);
+                    int result = rnd.Next(1, 10);
+                    data[i, j] = result;
+                    Console.Write(result + " ");
                 }
-                else
-                {
-                    orLess.Add(num);
-                }
-            }
-
-            Console.WriteLine();
-            Console.Write($"平均値より大きい数 : ");
-            foreach (int num2 in orMore)
-            {
-                Console.Write(num2 + " ");
-            }
-
-            Console.WriteLine();
-            Console.Write($"平均値より小さい数 : ");
-            foreach (int num3 in orLess)
-            {
-                Console.Write(num3 + " ");
+                Console.WriteLine();
             }
         }
     }
