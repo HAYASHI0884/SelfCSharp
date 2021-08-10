3e446e1435ba05fa19f6edd5fa8eab000b0fd642
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-10 09:57:23 +0900

    whileの無限ループ整数入力作成

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -6,58 +6,15 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("数値を入力して下さい。");
-            var num = int.Parse(Console.ReadLine());
+            int num = 0;
 
-            for (int i = 1; i <= num; i++)
+            while (0 <= num)
             {
-                if (num != i)
-                {
-                    Console.Write("■ ");
-                }
-                else
-                {
-                    Console.WriteLine("■ ");
-                }
+                Console.WriteLine("0以上の数値を入力して下さい。");
+                num = int.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine("数値を入力して下さい。");
-            var num2 = int.Parse(Console.ReadLine());
-            int i2 = 1;
-
-            while (i2 <= num2)
-            {
-                if (num2 != i2)
-                {
-                    Console.Write("■ ");
-                }
-                else
-                {
-                    Console.WriteLine("■ ");
-                }
-
-                i2++;
-
-            }
-
-            Console.WriteLine("数値を入力して下さい。");
-            var num3 = int.Parse(Console.ReadLine());
-            int i3 = 1;
-
-            do
-            {
-                if (num3 != i3)
-                {
-                    Console.Write("■ ");
-                }
-                else
-                {
-                    Console.WriteLine("■ ");
-                }
-
-                i3++;
-
-            } while (i3 <= num3);
+            Console.WriteLine("終了します");
         }
     }
 }
