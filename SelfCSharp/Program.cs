95431f1e6a90eac93b2f80cb0f9b115fd0157eab
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-10 14:58:48 +0900

    listの最大最小平均

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -10,39 +10,18 @@ namespace SelfCSharp
         {
             Random rnd = new Random();
             var list = new List<int>(10);
-            var orMore = new List<int>();
-            var orLess = new List<int>();
 
             for (int i = 0; i < list.Capacity; i++)
             {
-                int result = rnd.Next(1, 101);
+                int result = rnd.Next(1, 11);
                 list.Add(result);
                 Console.Write($"{result} ");
-
-                if (50 <= result)
-                {
-                    orMore.Add(result);
-                }
-                else
-                {
-                    orLess.Add(result);
-                }
             }
 
             Console.WriteLine();
-            Console.Write($"50以上の数 : ");
-            foreach (int num in orMore)
-            {
-                Console.Write(num + " ");
-            }
-
-            Console.WriteLine();
-            Console.Write($"50未満の数 : ");
-            foreach (int num2 in orLess)
-            {
-                Console.Write(num2 + " ");
-            }
-
+            Console.WriteLine($"最大値 : {list.Max()}");
+            Console.WriteLine($"最小値 : {list.Min()}");
+            Console.WriteLine($"平均値 : {list.Average()}");
         }
     }
 }
