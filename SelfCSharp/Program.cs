e6421521857819836a7e8d6c2b9262f3545458bf
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-10 15:32:38 +0900

    配列の星さ３問題

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -9,7 +9,9 @@ namespace SelfCSharp
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            var list = new List<int>(10);
+            var list = new List<int>(5);
+            var orMore = new List<int>();
+            var orLess = new List<int>();
 
             for (int i = 0; i < list.Capacity; i++)
             {
@@ -18,10 +20,37 @@ namespace SelfCSharp
                 Console.Write($"{result} ");
             }
 
+            int listAverage = (int)list.Average();
+
             Console.WriteLine();
-            Console.WriteLine($"最大値 : {list.Max()}");
-            Console.WriteLine($"最小値 : {list.Min()}");
-            Console.WriteLine($"平均値 : {list.Average()}");
+            Console.WriteLine($"合計値 : {list.Sum()}");
+            Console.WriteLine($"平均値 : {listAverage}");
+
+            foreach (int num in list)
+            {
+                if (listAverage < num)
+                {
+                    orMore.Add(num);
+                }
+                else
+                {
+                    orLess.Add(num);
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write($"平均値より大きい数 : ");
+            foreach (int num2 in orMore)
+            {
+                Console.Write(num2 + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write($"平均値より小さい数 : ");
+            foreach (int num3 in orLess)
+            {
+                Console.Write(num3 + " ");
+            }
         }
     }
 }
