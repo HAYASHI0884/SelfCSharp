4cbe80059aa544fb3b641a2ec7ef5daafc4975cd
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-10 11:42:03 +0900

    ループ文でずけいさ図形作成

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -8,11 +8,37 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 9; i++)
+            for (int i = 1; i <= 10; i++)
             {
-                for (int f = 1; f <= 9; f++)
+                for (int f = 1; f <= 10; f++)
                 {
-                    Console.Write(f + "*" + i + "=" + i * f + " ");
+                    if (f <= i)
+                    {
+                        Console.Write("■ ");
+                    }
+                    else
+                    {
+                        Console.Write("□ ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int f = 1; f <= 10; f++)
+                {
+                    if (f != i)
+                    {
+                        Console.Write("■ ");
+                    }
+                    else
+                    {
+                        Console.Write("□ ");
+                    }
                 }
 
                 Console.WriteLine();
