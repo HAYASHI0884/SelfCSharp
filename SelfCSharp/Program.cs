de3712626222fb0a8477dafe213fc4c693eb7af0
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-11 17:34:07 +0900

    分数問題

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -10,16 +10,71 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+            Random rnd = new Random();
+            int molecule = rnd.Next(1, 10);
+            int molecule2 = rnd.Next(1, 10);
+            int denominator = rnd.Next(2, 10);
+            int denominator2 = rnd.Next(2, 10);
+
+            double mol = molecule * denominator2 + molecule2 * denominator;
+            double den = denominator * denominator2;
+
+            Console.WriteLine($"{molecule}/{denominator} + {molecule2}/{denominator2}");
+            Console.WriteLine($"{mol}/{den}");
+
+            if (mol == den)
             {
-                if (i % 3 == 0 || (30 <= i && i < 40) || i.ToString().Contains("3") == true)
+                Console.WriteLine(1);
+            }
+            else if (mol > den)
+            {
+                if (mol % denominator == 0)
                 {
-                    Console.WriteLine("☆");
+                    mol /= denominator;
+                    den /= denominator;
                 }
-                else
+                else if (mol % denominator2 == 0)
                 {
-                    Console.WriteLine(i);
+                    mol /= denominator2;
+                    den /= denominator2;
                 }
+                else if (mol % 3 == 0 && den % 3 == 0)
+                {
+                    mol /= 3;
+                    den /= 3;
+                }
+                else if (mol % 2 == 0 && den % 2 == 0)
+                {
+                    mol /= 2;
+                    den /= 2;
+                }
+                Console.WriteLine($"{mol}/{den}");
+                mol /= den;
+                Console.WriteLine($"{mol}/{den}");
+            }
+            else
+            {
+                if (mol % denominator == 0)
+                {
+                    mol /= denominator;
+                    den /= denominator;
+                }
+                else if (mol % denominator2 == 0)
+                {
+                    mol /= denominator2;
+                    den /= denominator2;
+                }
+                else if (mol % 3 == 0 && den % 3 == 0)
+                {
+                    mol /= 3;
+                    den /= 3;
+                }
+                else if (mol % 2 == 0 && den % 2 == 0)
+                {
+                    mol /= 2;
+                    den /= 2;
+                }
+                Console.WriteLine($"{mol}/{den}");
             }
         }
     }
