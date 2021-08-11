8079f9a1fff22b826071b4728a903e0195742ed0
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-11 13:45:39 +0900

    オブジェクト指向の数値問題

---
+++ SelfCSharp/Calculation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp
+{
+    class Calculation
+    {
+        private int number1;
+
+        private int number2;
+
+        public int Number1
+        {
+            set { number1 = value; }
+            get { return number1; }
+        }
+
+        public int Number2
+        {
+            set { number2 = value; }
+            get { return number2; }
+        }
+
+        public int Add()
+        {
+            return Number1 + number2;
+        }
+
+        public int Sub()
+        {
+            return Number1 - number2;
+        }
+    }
+}
