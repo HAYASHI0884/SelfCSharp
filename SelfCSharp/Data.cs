7b7141542886dd2f0a086240eac287418170a51e
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-11 11:18:15 +0900

    計算メソッドv2

---
+++ SelfCSharp/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp
+{
+    class Data
+    {
+        internal int Number = 0;
+        internal string Comment = "";
+    }
+}
--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -10,15 +10,11 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            //MinMax m = new MinMax();
-            //int a = 3, b = 2, c = 1;
-            //Console.WriteLine("{0}と{1}と{2}のうち、最大のものは{3}", a, b, c, m.Max(a, b, c));
-            //Console.WriteLine("{0}と{1}と{2}のうち、最小のものは{3}", a, b, c, m.Min(a, b, c));
-
-            Calc c = new Calc();
-            double a = 4.1, b = 2.3;
-            Console.WriteLine($"{a} + {b} = {c.Add(a, b)}");
-            Console.WriteLine($"{a} - {b} = {c.Sub(a, b)}");
+            Data d = new Data();
+            d.Number = 100;
+            d.Comment = "Programming C#";
+            Console.WriteLine("number = " + d.Number);
+            Console.WriteLine("comment = " + d.Comment);
         }
     }
 }
