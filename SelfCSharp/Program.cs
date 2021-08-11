4dd8ab669676cb56e61b330a7ce9135f84804df2
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-11 14:49:43 +0900

    ピタゴラス数問題

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -10,13 +10,25 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            Calculation c = new Calculation();
-            c.Number1 = 8;    //  一つ目の数をセット
-            c.Number2 = 9;    //  二つ目の数をセット
-            //  二つの数の和を表示
-            Console.WriteLine("{0} + {1} = {2}", c.Number1, c.Number2, c.Add());
-            //  二つの数の差を表示
-            Console.WriteLine("{0} - {1} = {2}", c.Number1, c.Number2, c.Sub());
+            var count = new List<string> { };
+
+            for (int a = 1; a <= 100; a++)
+            {
+                for (int b = 1; b <= 100; b++)
+                {
+                    for (int c = 1; c <= 100; c++)
+                    {
+                        if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2) && a < b)
+                        {
+                            string p = $"a = {a}, b = {b}, c = {c}";
+                            count.Add(p);
+                            Console.WriteLine(p);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(count.Count);
         }
     }
 }
