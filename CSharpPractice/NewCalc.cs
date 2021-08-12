e1644dbdaad079fbf9cf2b278e0d5ec78eca8e46
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-12 16:21:25 +0900

    計算APIの実装

---
+++ CSharpPractice/NewCalc.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpPractice
+{
+    class NewCalc : FundCalc
+    {
+        public double Mul()
+        {
+            return Number1 * Number2;
+        }
+
+        public double Div()
+        {
+            return Number1 / Number2;
+        }
+    }
+}
