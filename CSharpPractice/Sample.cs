3be926e58ada8f12820e52e866dec98a2fdc83a3
HAYASHI0884 <lin.jiangda884@gmail.com>
2021-08-12 10:35:03 +0900

    コンストラクタ練習

---
+++ CSharpPractice/Sample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    internal class Sample
+    {
+
+        public Sample()
+        {
+            Console.WriteLine("コンストラクタ");
+        }
+
+        public void foo()
+        {
+            Console.WriteLine("foo");
+        }
+    }
+}
